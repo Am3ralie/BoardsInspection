76e2bddb7ae938047cb63ef199eec7d6957088fa
bertosh785 <bertosh785@gmail.com>
2025-07-13 12:51:26 +0300

    Наполнение ApplicationDbContext для связи с бд
    Добавление через NuGet UI:
    1. Pomelo.EntityFrameworkCore.MySql 8.0.3
    2. Microsoft.EntityFrameworkCore.Design 8.0.3 (не 9 потому что они не мигрируют)
    
    Добавление строки подключения appsettings.json в .webAPI

---
+++ BoardsInspection.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace BoardsInspection.Infrastructure.Data
+//{
+//    internal class ApplicationDbContext
+//    {
+//    }
+//}
+
+using BoardsInspection.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace BoardsInspection.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Test> Tests { get; set; }
+        public DbSet<Event> Events { get; set; }
+    }
+}
